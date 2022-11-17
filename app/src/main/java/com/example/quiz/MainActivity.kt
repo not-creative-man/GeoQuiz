ba9921d54ac50e8e392293e0fb673c95bd5a22bc
not-creative-man <lebedev_i_s@inbox.ru>
2022-11-18 00:54:50 +0300

    added new functional

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.quiz
 
+import android.app.Activity
+import android.content.ClipData.newIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.*
+import androidx.activity.result.ActivityResult
+import java.io.DataOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,13 +31,17 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_mideast, false),
     )
 
+    private lateinit var mCheatButton: Button
+
     private var mCurrentIndex = 0
+    private var REQUEST_CODE_CHEAT = 0
+    private var mIsCheater: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(savedInstanceState != null) mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0)
+        if (savedInstanceState != null) mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0)
 
         Log.d(TAG, "onCreate()")
 
@@ -41,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         mFalseButton = findViewById(R.id.false_button)
         mNextButton = findViewById(R.id.next_button)
         mPrevButton = findViewById(R.id.prev_button)
+        mCheatButton = findViewById(R.id.cheat_button)
 
         updateQuestion()
 
@@ -50,40 +60,48 @@ class MainActivity : AppCompatActivity() {
         mFalseButton.setOnClickListener {
             checkAnswers(false)
         }
-        mNextButton.setOnClickListener{
+        mNextButton.setOnClickListener {
             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
             updateQuestion()
         }
-        mQuestionTextView.setOnClickListener{
+        mQuestionTextView.setOnClickListener {
             updateQuestion()
         }
-        mPrevButton.setOnClickListener{
-            mCurrentIndex = if(mCurrentIndex == 0) 6 else (mCurrentIndex - 1)
+        mPrevButton.setOnClickListener {
+            mCurrentIndex = if (mCurrentIndex == 0) 6 else (mCurrentIndex - 1)
             updateQuestion()
+        }
+        mCheatButton.setOnClickListener {
+            val answerIsTrue = mQuestionBank[mCurrentIndex].mAnswerTrue
+            val intent = CheatActivity.newIntent(this, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
     }
 
-     private fun updateQuestion(){
-         val question = mQuestionBank[mCurrentIndex].mTextResId
-         (mQuestionTextView as TextView?)?.setText(question)
-         changeClickable(mTrueButton, false)
-         changeClickable(mFalseButton, false)
+    private fun updateQuestion() {
+        val question = mQuestionBank[mCurrentIndex].mTextResId
+        (mQuestionTextView as TextView?)?.setText(question)
+        changeClickable(mTrueButton, false)
+        changeClickable(mFalseButton, false)
+        mIsCheater = false
     }
 
-    private fun checkAnswers(userPressedTrue: Boolean){
+    private fun checkAnswers(userPressedTrue: Boolean) {
         changeClickable(mTrueButton, true)
         changeClickable(mFalseButton, true)
-        if(userPressedTrue == mQuestionBank[mCurrentIndex].mAnswerTrue){
-            Toast.makeText(this@MainActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show()
-        }
-        else{
-            Toast.makeText(this@MainActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
-        }
+        var messageResId = if (mIsCheater)
+            R.string.judgment_toast
+        else if (userPressedTrue == mQuestionBank[mCurrentIndex].mAnswerTrue)
+            R.string.correct_toast
+        else
+            R.string.incorrect_toast
+
+        Toast.makeText(this@MainActivity, messageResId, Toast.LENGTH_SHORT).show()
     }
 
-    fun changeClickable(button: Button, isChecking: Boolean){
+    fun changeClickable(button: Button, isChecking: Boolean) {
         button.isClickable = !isChecking
-        if(button.isClickable) button.setBackgroundColor(getColor(R.color.purple_500))
+        if (button.isClickable) button.setBackgroundColor(getColor(R.color.purple_500))
         else button.setBackgroundColor(getColor(R.color.grey))
         Log.d("TAG", button.background.toString())
         Log.d("TAG", button.isClickable.toString())
@@ -118,6 +136,19 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy()")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data)
+        }
     }
 }
 
